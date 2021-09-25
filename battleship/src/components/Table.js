fc74b4c59f38208e870f17817b345ff8e89cb43c
gurumi44 <gurumikarumi@gmail.com>
2021-09-25 22:26:49 +0700

    Index

--- battleship/src/components/Table.js
+++ battleship/src/components/Table.js
@@ -1,4 +1,3 @@
-import Image from './Image'
 import Container from 'react-bootstrap/Container'
 import { useEffect, useState } from 'react'
 
@@ -7,6 +6,7 @@ const Table = (props) => {
     const player = Object.freeze({
         name: props.player,
         ship: Array.from([]),
+        position: Array.from([]),
         founded: 0,
     })
     const [playerState, setPlayerState] = useState(player)
@@ -43,14 +43,21 @@ const Table = (props) => {
     }
 
     function checkShip(e) {
-        console.log(parseInt(e.target.id))
-        console.log(playerState)
-        if(playerState.ship.includes(parseInt(e.target.id))){
+        var clicked = [parseInt(e.target.id[0]), parseInt(e.target.id[2])]
+        console.log(clicked)
+        console.log(playerState.ship[0])
+        var found = playerState.ship.find(ship => {
+            if((ship[0] === clicked[0]) && (ship[1] === clicked[1])){
+                return true
+            }
+            else return false
+        })
+        if(found){
             setPlayerState({
                 ...playerState,
                 founded: playerState.founded + 1
             })
-            if(playerState.founded == 10) {
+            if(playerState.founded === 10) {
                 gameover = true
             }
             foundShip(e)
@@ -74,29 +81,67 @@ const Table = (props) => {
         console.log(playerState.name + 'WIN!!!')
     }
 
-    function generateShip() {
-        var temp = []
-        for(let i = 0;i < 10;i++){
-            temp.push(randomShip(0, 99, temp))
+    function generateShip(size) {
+        // Random => Check available => random available
+        // If not available => Random again
+        var randomIndex = []
+        var checkAvailable = []
+        var ship = []
+
+        // Random Ship HEAD
+        do {
+            randomIndex.splice(0, randomIndex.length)
+            for(let i = 0;i < 2;i++){
+                randomIndex.push(randomShip(0, 9, randomIndex))
+            }
+        } while ((randomIndex[0] + size > 9) && (randomIndex[0] - size < 0) && 
+        (randomIndex[1] + size > 9) && (randomIndex[1] - size < 0))
+
+        // Check available เช็คพื้นที่พอไหม 4 ทาง first num: row, y axis second num: col, x axis
+        for(let i = 1;i < size;i++){
+            if(!(randomIndex[0] + size > 9)){
+                checkAvailable.push([randomIndex[0] + i, randomIndex[1]])
+            }
+            if(!(randomIndex[0] - size < 0)){
+                checkAvailable.push([randomIndex[0] - i, randomIndex[1]])
+            }
+            if(!(randomIndex[1] + size > 9)){
+                checkAvailable.push([randomIndex[0], randomIndex[1] + i])
+            }
+            if(!(randomIndex[1] - size < 0)){
+                checkAvailable.push([randomIndex[0], randomIndex[1] - i])
+            }
         }
+
+        // เช็คว่าระหว่างทางมีเรือไหม ถ้ามี ลบทางนั้นทิ้ง 
+        checkAvailable.forEach(path => {
+            playerState.ship.find(positioned => {
+                if(positioned[0] === path[0]){
+
+                }
+                
+            })
+        })
+        
+        ship.push(randomIndex)
         setPlayerState({
             ...playerState,
-            ship: temp
+            ship: ship
         })
-        console.log(playerState.ship)
+        console.log(randomIndex)
+        console.log(checkAvailable)
     }
 
     useEffect(() => {
-        generateShip()
+        generateShip(2)
     }, [setPlayerState])
 
     const createCell = (id, src) => {
 
-
-        if (id < 10){
+        if (id[0] === 0){
             return( 
                     <th>
-                        {id + 1} 
+                        {id[1] + 1} 
                         <td className='cell'>
                             <img id={id} src={src} onClick={play} />
                         </td>
@@ -125,7 +170,7 @@ const Table = (props) => {
                             </th>
                             {Array.from({ length: 10 }).map((_, col) => {
                                 var index = ((10 - row - 1) * 10) + (10 - col)
-                                var id = (row * 10) + col
+                                var id = [row, col]
                                 var src = 'map/' + index + '.jpg'
                                 return(
                                     createCell(id, src)
